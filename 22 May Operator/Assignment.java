db4ce654971fb0078a02efe47a5f737aac154c39
Sarveshsdeshmukh <sarveshsdeshmukh@gmail.com>
2023-09-17 11:19:17 +0530

    Upto Relational video

---
+++ 22 May Operator/Assignment.java
@@ -0,0 +1,11 @@
+public class Assignment {
+    public static void main(String args[]){ 
+        int m1 , m2 , m3 , m4 ;
+        m1 = m2 = m3 = m4 = 34;
+        System.out.println(m1 + " " + m2 + " " + m3 + " " + m4 );
+        m1 += 10; // m1 = m1 + 10
+        System.out.println(m1);
+        m2 -= 34;
+        System.out.println(m2);
+    }    
+}
---
+++ 22 May Operator/Relational.java
@@ -0,0 +1,18 @@
+public class Relational {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 10;
+        //Many more eamples you can try yourself 
+
+        //Relatinal oprator are as follows == <= >= != 
+
+        if(a == b && a != b) {
+            a +=b;
+            System.out.println(a);
+        }else{
+            System.out.println(a*4);
+            System.out.println("This is Else block !");
+            System.out.println("Don't touch to the if block!");
+        }
+    }    
+}
