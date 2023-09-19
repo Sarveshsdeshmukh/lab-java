911c03a08bba3b5fa0f5c0ec89d8bcd108283cd9
Sarveshsdeshmukh <sarveshsdeshmukh@gmail.com>
2023-09-19 22:36:07 +0530

    Ternery Updated

--- 22 May Operator/Ternery.java
+++ 22 May Operator/Ternery.java
@@ -2,7 +2,6 @@ public class Ternery{
     public static void main(String args[]){
         int a = 10 ;
         int b = 20 ; 
-        int c = 30 ;
 
         System.out.println("Hello I am using the ternery operator today !");
 
