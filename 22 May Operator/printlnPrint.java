ec27963b8dd8f10bffe45962b7f146f28e6a9a1d
Sarveshsdeshmukh <sarveshsdeshmukh@gmail.com>
2023-09-28 21:03:47 +0530

    Upto Pattern 2

---
+++ 22 May Operator/printlnPrint.java
@@ -0,0 +1,10 @@
+public class printlnPrint {
+    public static void main(String args[]){
+        String name = "Jay Shree Ram ";
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println("-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0");
+        System.out.print(name+" || "+name);
+    }
+}
