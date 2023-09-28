4177c98138e244c141b18d9ac5dfe7ebe16783d4
Sarveshsdeshmukh <sarveshsdeshmukh@gmail.com>
2023-09-28 21:04:05 +0530

    Upto Pattern 2

---
+++ 22 May Operator/Pattern1.java
@@ -0,0 +1,13 @@
+public class Pattern1 {
+    public static void main(String args[]){
+        int n = 5;
+        int m = 10;
+
+     for(int j = 0 ; j < m ; j++){   
+        for(int i = 0 ; i < n ; i++){
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+    }
+    }
+}
